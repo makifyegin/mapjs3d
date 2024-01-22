5f16457def1ebad98783a7788708a0eeeda3cf82
makifyegin <mehmetakifyegin@icloud.com>
2024-01-22 00:34:39 +0000

    Change some initial codes

--- index.ts
+++ index.ts
@@ -13,30 +13,41 @@ import {
   WebGLRenderer,
 } from "three";
 
+
+
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 let map: google.maps.Map;
 
 const mapOptions = {
-  tilt: 0,
+  tilt:0,
   heading: 0,
   zoom: 18,
-  center: { lat: 35.6594945, lng: 139.6999859 },
+  center: { lat: 50.2746, lng: -4.7917 },
   mapId: "15431d2b469f209e",
   // disable interactions due to animation loop and moveCamera
   disableDefaultUI: true,
-  gestureHandling: "none",
-  keyboardShortcuts: false,
+  gestureHandling: "cooperative",
+  keyboardShortcuts: true,
 };
+
+const locations = [
+  { lat: 50.2746, lng: -4.7917 },
+  { lat: 10.2750, lng: -1.7920 },
+  { lat: 30.2752, lng: -60.7915 },
+  { lat: 0.2748, lng: -2.7912 },
+  { lat: 150.2749, lng: -23.7919 },
+];
 
 function initMap(): void {
   const mapDiv = document.getElementById("map") as HTMLElement;
   map = new google.maps.Map(mapDiv, mapOptions);
   initWebglOverlayView(map);
 }
-
+/*
 function initWebglOverlayView(map: google.maps.Map): void {
   let scene, renderer, camera, loader;
+  let isCameraAnimating = true;
   const webglOverlayView = new google.maps.WebGLOverlayView();
 
   webglOverlayView.onAdd = () => {
@@ -46,20 +57,21 @@ function initWebglOverlayView(map: google.maps.Map): void {
 
     camera = new PerspectiveCamera();
 
-    const ambientLight = new AmbientLight(0xffffff, 0.75); // Soft white light.
+    const ambientLight = new AmbientLight(0xffffff, 0.90); // Soft white light.
     scene.add(ambientLight);
 
     const directionalLight = new DirectionalLight(0xffffff, 0.25);
-    directionalLight.position.set(0.5, -1, 0.5);
+    directionalLight.position.set(0, 0, 0);
     scene.add(directionalLight);
 
     // Load the model.
     loader = new GLTFLoader();
-    const source =
-      "https://raw.githubusercontent.com/googlemaps/js-samples/main/assets/pin.gltf";
+    const source = "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/main/2.0/Duck/glTF-Embedded/Duck.gltf";
     loader.load(source, (gltf) => {
-      gltf.scene.scale.set(10, 10, 10);
+      gltf.scene.scale.set(25, 25, 25);
       gltf.scene.rotation.x = Math.PI; // Rotations are in radians.
+      gltf.scene.rotation.set(Math.PI / 2, 0, 0);
+      
       scene.add(gltf.scene);
     });
   };
@@ -112,7 +124,107 @@ function initWebglOverlayView(map: google.maps.Map): void {
     renderer.resetState();
   };
   webglOverlayView.setMap(map);
+
+    map.addListener("click", () => {
+    isCameraAnimating = false;
+  });
 }
+*/
+function initWebglOverlayView(map: google.maps.Map): void {
+  let scene, renderer, camera;
+  let isCameraAnimating = true;
+  const webglOverlayView = new google.maps.WebGLOverlayView();
+
+  // List of different locations
+  const locations = [
+    { lat: 150.2746, lng: -4.7917 },
+    { lat: 90.2750, lng: -24.7920 },
+    { lat: 50.2752, lng: -34.7915 },
+    { lat: 50.2748, lng: -4.7912 },
+    { lat: 50.2749, lng: -4.7919 },
+  ];
+
+  webglOverlayView.onAdd = () => {
+    scene = new Scene();
+    camera = new PerspectiveCamera();
+
+    const ambientLight = new AmbientLight(0xffffff, 0.90);
+    scene.add(ambientLight);
+
+    const directionalLight = new DirectionalLight(0xffffff, 0.25);
+    directionalLight.position.set(0, 0, 0);
+    scene.add(directionalLight);
+
+    // Load the model for each location
+    const loader = new GLTFLoader();
+    locations.forEach((location, index) => {
+      loader.load("https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/main/2.0/Duck/glTF-Embedded/Duck.gltf", (gltf) => {
+        gltf.scene.scale.set(25, 25, 25);
+       // gltf.scene.rotation.x = Math.PI;
+        gltf.scene.rotation.set(Math.PI / 2, 0, 0);
+        gltf.scene.position.set(location.lng, location.lat, 0); // Set the position
+        scene.add(gltf.scene);
+      });
+    });
+  };
+
+  webglOverlayView.onContextRestored = ({ gl }) => {
+    renderer = new WebGLRenderer({
+      canvas: gl.canvas,
+      context: gl,
+      ...gl.getContextAttributes(),
+    });
+    renderer.autoClear = false;
+
+    const loader = new GLTFLoader();
+    const onLoad = () => {
+      renderer.setAnimationLoop(() => {
+        webglOverlayView.requestRedraw();
+        const { tilt, heading, zoom } = mapOptions;
+        map.moveCamera({ tilt, heading, zoom });
+
+        if (mapOptions.tilt < 67.5) {
+          mapOptions.tilt += 0.5;
+        } else if (mapOptions.heading <= 360) {
+          mapOptions.heading += 0.2;
+          mapOptions.zoom -= 0.0005;
+        } else {
+          renderer.setAnimationLoop(null);
+        }
+      });
+    };
+
+    // Wait to move the camera until all 3D models load
+    loader.manager.onLoad = onLoad;
+    locations.forEach(() => {
+      loader.load("path-to-your-model.gltf", () => {});
+    });
+  };
+
+  webglOverlayView.onDraw = ({ gl, transformer }): void => {
+    locations.forEach((location, index) => {
+      const latLngAltitudeLiteral: google.maps.LatLngAltitudeLiteral = {
+        lat: location.lat,
+        lng: location.lng,
+        altitude: 100,
+      };
+
+      const matrix = transformer.fromLatLngAltitude(latLngAltitudeLiteral);
+      camera.projectionMatrix = new Matrix4().fromArray(matrix);
+
+      webglOverlayView.requestRedraw();
+      renderer.render(scene, camera);
+      renderer.resetState();
+    });
+  };
+
+  webglOverlayView.setMap(map);
+
+  map.addListener("click", () => {
+    isCameraAnimating = false;
+  });
+}
+
 
 declare global {
   interface Window {
